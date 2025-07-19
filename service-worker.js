76513ac58a008bcbe83ea423bc5a3f9a3fedf0bd
Ahmed Sharyph <39231530+AhmedSharyph@users.noreply.github.com>
2025-07-19 15:52:41 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,19 @@
-self.addEventListener('install', e => {
-  console.log('Service Worker installed');
+const CACHE_NAME = 'funatrace-v1';
+const ASSETS_TO_CACHE = [
+  '/p/funatrace-v3.html',
+  'https://ahmedsharyph.github.io/funatrace/ft_logo.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
